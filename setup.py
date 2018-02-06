31bcebc73c17244d50efc285c5cc71bc56926f0b
Ugne <ugne@studyportals.com>
2018-02-06 10:26:30 +0100

    Made this project accessable through python 2.7 pip install

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name='sl_client',
-    version='1.1',
+    version='1.2',
     py_modules=['sl_client'],
     license='BSD 3-Clause',
     description="A Python tool that connects to the Studyportals ServiceLayer using Studyportals reflector and retrieves data.",
     url='https://github.com/studyportals/ServiceLayerClient-python',
     author='Ugne Laima Ciziute',
     author_email='devops@studyportals.com',
-    install_requires=['requests'],
-	python_requires='>=3'
+    install_requires=['requests']
 )
